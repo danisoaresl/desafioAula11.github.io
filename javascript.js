da4f99efd618eb709ea801449705d5ad1c65ecd5
danisoaresl <daniele.soaresl@gmail.com>
2023-11-22 14:36:37 -0300

    atualizacao2211

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+ 
+var numero = parseInt(prompt("Digite um número inteiro:"));
+
+if (numero % 2 === 0 && numero > 0 && numero < 100) {
+    alert("O número atende aos critérios.");
+}   else {
+    alert("O número não atende aos critérios.");
+} 
