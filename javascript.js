529e1acb501c1e8fe4175f88d448adc36453b31f
danisoaresl <daniele.soaresl@gmail.com>
2023-11-28 15:45:28 -0300

    atualizacao2811

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
 
-var n = parseInt(prompt('Digite um número: '));
+function bodyLoad() {
+    var numero = parseInt(prompt('Digite um número inteiro: '));
 
-if(n > 0 && n < 100 && n % 2 == 0){
-
-    alert('O número atendeu os critérios');
-}else{
-    alert('O número não atendeu os critérios');
+    if(numero > 0 && numero < 100 && numero % 2 === 0){
+    
+        alert('O número atendeu aos critérios.');
+    }else{
+        alert('O número não atendeu aos critérios.');
+    }
+   
 }
-
-
